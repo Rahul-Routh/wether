2e688bb98a8878c482f5a46ef0f33ca691fd82ae
rahulrouth <its.rahulrouth@gmail.com>
2021-01-18 13:23:29 +0530

    splash screen

--- app/src/main/java/com/purpuligo/weather/MainActivity.java
+++ app/src/main/java/com/purpuligo/weather/MainActivity.java
@@ -2,18 +2,23 @@ package com.purpuligo.weather;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
 public class MainActivity extends AppCompatActivity {
 
+
     private WebView webView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
 
         webView = (WebView) findViewById(R.id.webview);
         webView.setWebViewClient(new WebViewClient());
